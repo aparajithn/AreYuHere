68ca00e08c56be13a4492ea08ccf0e8e6ce44dc2
aparajithn <nagarajan.aparajith@gmail.com>
2020-05-21 19:48:12 -0400

    Added Google Signup with Firebase Auth

--- app/src/main/java/com/example/areyuhere/SignInFragment.kt
+++ app/src/main/java/com/example/areyuhere/SignInFragment.kt
@@ -1,5 +1,6 @@
 package com.example.areyuhere
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,28 +14,35 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
+
 
 private const val TAG = "SigninFragment"
 
-class SignInFragment :Fragment(){
+class SignInFragment :Fragment() {
     private lateinit var loginButton: Button
-    private lateinit var username:EditText
-    private lateinit var password:EditText
+    private lateinit var username: EditText
+    private lateinit var password: EditText
     private lateinit var auth: FirebaseAuth
-    private lateinit var signupTextView:TextView
+    private lateinit var signupTextView: TextView
+
+
     var teacher_email = ""
     var teacherEmails = mutableListOf<String>()
     private var counter = 0
     var flagIsTeacher = false
+
 
     val viewModel: UserViewModel by activityViewModels()
 
@@ -43,18 +51,19 @@ class SignInFragment :Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-            val view = inflater.inflate(R.layout.fragment_signin, container, false)
-        loginButton =view.findViewById(R.id.login_button)
+        val view = inflater.inflate(R.layout.fragment_signin, container, false)
+        loginButton = view.findViewById(R.id.login_button)
         username = view.findViewById(R.id.username)
         password = view.findViewById(R.id.password)
         signupTextView = view.findViewById(R.id.signup_link)
         auth = FirebaseAuth.getInstance()
 
+
         viewModel.getStatus.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 viewModel.children_count = dataSnapshot.childrenCount
                 Log.d(TAG, viewModel.children_count.toString())
-                if (viewModel.userList.isNullOrEmpty()){
+                if (viewModel.userList.isNullOrEmpty()) {
                     for (snapshot in dataSnapshot.children) {
                         val user = User()
                         user.id = snapshot.key.toString()
@@ -79,8 +88,7 @@ class SignInFragment :Fragment(){
         viewModel.teacherListRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (snapshot in dataSnapshot.children) {
-                    for (snapshot2 in snapshot.children)
-                    {
+                    for (snapshot2 in snapshot.children) {
                         if (snapshot2.key.toString() == "email") {
                             teacher_email = snapshot2.value.toString()
                             Log.d(TAG, teacher_email)
@@ -90,13 +98,15 @@ class SignInFragment :Fragment(){
 
                 }
             }
+
             override fun onCancelled(databaseError: DatabaseError) {}
         })
 
         loginButton.setOnClickListener {
             if (!username.text.toString().isNullOrEmpty() && !password.text.toString()
-                    .isNullOrEmpty()){
-                    auth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
+                    .isNullOrEmpty()
+            ) {
+                auth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
                     .addOnCompleteListener(requireActivity()) { task ->
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
@@ -106,8 +116,7 @@ class SignInFragment :Fragment(){
 
                             val teachersIterator = teacherEmails.iterator()
                             flagIsTeacher = false
-                            while (teachersIterator.hasNext())
-                            {
+                            while (teachersIterator.hasNext()) {
                                 if (username.text.toString().equals(teachersIterator.next())) {
                                     flagIsTeacher = true
                                 }
@@ -116,14 +125,11 @@ class SignInFragment :Fragment(){
                                 Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_teacherHomeFragment)
                                 view.findNavController()
                                     .navigate(R.id.action_signInFragment_to_teacherHomeFragment)
-                            }
-                            else
-                            {
+                            } else {
                                 Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_studentHomeFragment)
                                 view.findNavController()
                                     .navigate(R.id.action_signInFragment_to_studentHomeFragment)
                             }
-
 
 
                         } else {
@@ -136,20 +142,22 @@ class SignInFragment :Fragment(){
                         }
                     }
 
-            }
-            else{
+            } else {
                 Toast.makeText(
-                    context, "Enter a username & password",
-                    Toast.LENGTH_SHORT)
+                        context, "Enter a username & password",
+                        Toast.LENGTH_SHORT
+                    )
                     .show()
             }
         }
-        signupTextView.setOnClickListener{
+        signupTextView.setOnClickListener {
             Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_signUpFragment)
             view.findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
         }
+
+
+
         return view
 
     }
-
 }
--- app/src/main/java/com/example/areyuhere/SignUpFragment.kt
+++ app/src/main/java/com/example/areyuhere/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.example.areyuhere
 
 import android.R.attr.name
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,7 +15,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 
 
@@ -28,6 +35,8 @@ class SignUpFragment:Fragment() {
     private lateinit var signup_button:Button
     private lateinit var teacher_slider:Switch
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignIn: SignInButton
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     val viewModel: UserViewModel by activityViewModels()
 
@@ -43,7 +52,15 @@ class SignUpFragment:Fragment() {
         displayName_edittext = view.findViewById(R.id.displayname)
         signup_button = view.findViewById(R.id.signup_button)
         teacher_slider = view.findViewById(R.id.teacher_slider)
+        googleSignIn = view.findViewById(R.id.google_signin)
         auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+
+        googleSignInClient = GoogleSignIn.getClient(context!!, gso)
 
 
         signup_button.setOnClickListener {
@@ -58,7 +75,11 @@ class SignUpFragment:Fragment() {
                     .navigate(R.id.action_signUpFragment_to_signInFragment)
             }
         }
-        return view
+        googleSignIn.setOnClickListener {
+            signIn()
+        }
+
+            return view
     }
     //Add user to firebase auth
     fun signup_auth(){
@@ -93,5 +114,49 @@ class SignUpFragment:Fragment() {
             viewModel.getStatus.child(index.toString()).updateChildren(userData)
         }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // [START_EXCLUDE]
 
+                // [END_EXCLUDE]
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        // [START_EXCLUDE silent]
+        // [END_EXCLUDE]
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure")
+                }
+
+            }
+    }
+    // [END auth_with_google]
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
 }
+
